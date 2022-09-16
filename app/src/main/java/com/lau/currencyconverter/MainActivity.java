cfe494d55d2c0e1838ea2fccd1942aae38b7cd5e
anthonyykh <anthonykh01.ak@gmail.com>
2022-09-16 20:03:37 +0300

    added second page with if condition

--- app/src/main/java/com/lau/currencyconverter/MainActivity.java
+++ app/src/main/java/com/lau/currencyconverter/MainActivity.java
@@ -49,8 +49,7 @@ public class MainActivity extends AppCompatActivity {
 //            toast.show();
 //
 //        }
-        Intent intent = new Intent(this, page1.class);
-        startActivity(intent);
+
     }
 
 }
--- app/src/main/java/com/lau/currencyconverter/page1.java
+++ app/src/main/java/com/lau/currencyconverter/page1.java
@@ -2,13 +2,32 @@ package com.lau.currencyconverter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class page1 extends AppCompatActivity {
-
+    EditText username;
+    EditText password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page1);
+
+        username=(EditText) findViewById(R.id.username);
+        password=(EditText) findViewById(R.id.password);
     }
+
+    public void login(View v) {
+        String username1=username.getText().toString();
+        String password1=password.getText().toString();
+        if (username1=="user.leb"&&password1=="123456"){
+            Intent intent = new Intent(this, page1.class);
+            startActivity(intent);
+
+        }
+    }
+
+
 }
