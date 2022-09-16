3a5854d54be952c1714d2022c814be5c3c52723e
anthonyykh <anthonykh01.ak@gmail.com>
2022-09-16 19:52:40 +0300

    added second page

--- app/src/main/java/com/lau/currencyconverter/MainActivity.java
+++ app/src/main/java/com/lau/currencyconverter/MainActivity.java
@@ -2,6 +2,7 @@ package com.lau.currencyconverter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -27,27 +28,29 @@ public class MainActivity extends AppCompatActivity {
         usd_rate=(EditText) findViewById(R.id.usd_rate);
     }
     public void Pressed_button(View v) {
-        String lbp_input=lbp_rate.getText().toString();
-        String usd_input=usd_rate.getText().toString();
-
-        if (lbp_input!=null&&usd_input.isEmpty()) {
-            int result = Integer.parseInt(lbp_input) /40000;
-            Toast toast = Toast.makeText(getApplicationContext(), "the equivalent to this amount in usd is :" + result, Toast.LENGTH_LONG);
-            toast.show();
-        }
-
-        else if(usd_input!=null&&lbp_input.isEmpty()){
-            int result=Integer.parseInt(usd_input)*40000;
-            Toast toast=Toast.makeText(getApplicationContext(),"the equivalent to this amount in lbp is :"+result, Toast.LENGTH_LONG);
-            toast.show();
-        }
-        else if(lbp_input!=null&&usd_input!=null)
-        {
-
-            Toast toast=Toast.makeText(getApplicationContext(),"Invalid input please fill only one of the boxes ", Toast.LENGTH_LONG);
-            toast.show();
-
-        }
+//        String lbp_input=lbp_rate.getText().toString();
+//        String usd_input=usd_rate.getText().toString();
+//
+//        if (lbp_input!=null&&usd_input.isEmpty()) {
+//            int result = Integer.parseInt(lbp_input) /40000;
+//            Toast toast = Toast.makeText(getApplicationContext(), "the equivalent to this amount in usd is :" + result, Toast.LENGTH_LONG);
+//            toast.show();
+//        }
+//
+//        else if(usd_input!=null&&lbp_input.isEmpty()){
+//            int result=Integer.parseInt(usd_input)*40000;
+//            Toast toast=Toast.makeText(getApplicationContext(),"the equivalent to this amount in lbp is :"+result, Toast.LENGTH_LONG);
+//            toast.show();
+//        }
+//        else if(lbp_input!=null&&usd_input!=null)
+//        {
+//
+//            Toast toast=Toast.makeText(getApplicationContext(),"Invalid input please fill only one of the boxes ", Toast.LENGTH_LONG);
+//            toast.show();
+//
+//        }
+        Intent intent = new Intent(this, page1.class);
+        startActivity(intent);
     }
 
 }
