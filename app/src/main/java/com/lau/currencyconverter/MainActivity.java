cc20dbd44709f91525c3218531ed97c7606db2ba
anthonyykh <anthonykh01.ak@gmail.com>
2022-09-16 18:51:46 +0300

    added edit text and variable names for the variables taken from the ui

--- app/src/main/java/com/lau/currencyconverter/MainActivity.java
+++ app/src/main/java/com/lau/currencyconverter/MainActivity.java
@@ -5,10 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    EditText lbp_rate;
+    EditText usd_rate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lbp_rate=(EditText) findViewById(R.id.lbp_rate);
+        usd_rate=(EditText) findViewById(R.id.usd_rate);
     }
+
+
 }
