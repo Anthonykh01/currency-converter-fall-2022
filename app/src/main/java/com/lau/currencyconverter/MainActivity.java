7b04b83590000982990c7d33313a8590b4840b11
anthonyykh <anthonykh01.ak@gmail.com>
2022-09-16 20:14:39 +0300

    fixed .equals error an added else statement

--- app/src/main/java/com/lau/currencyconverter/MainActivity.java
+++ app/src/main/java/com/lau/currencyconverter/MainActivity.java
@@ -28,27 +28,27 @@ public class MainActivity extends AppCompatActivity {
         usd_rate=(EditText) findViewById(R.id.usd_rate);
     }
     public void Pressed_button(View v) {
-//        String lbp_input=lbp_rate.getText().toString();
-//        String usd_input=usd_rate.getText().toString();
-//
-//        if (lbp_input!=null&&usd_input.isEmpty()) {
-//            int result = Integer.parseInt(lbp_input) /40000;
-//            Toast toast = Toast.makeText(getApplicationContext(), "the equivalent to this amount in usd is :" + result, Toast.LENGTH_LONG);
-//            toast.show();
-//        }
-//
-//        else if(usd_input!=null&&lbp_input.isEmpty()){
-//            int result=Integer.parseInt(usd_input)*40000;
-//            Toast toast=Toast.makeText(getApplicationContext(),"the equivalent to this amount in lbp is :"+result, Toast.LENGTH_LONG);
-//            toast.show();
-//        }
-//        else if(lbp_input!=null&&usd_input!=null)
-//        {
-//
-//            Toast toast=Toast.makeText(getApplicationContext(),"Invalid input please fill only one of the boxes ", Toast.LENGTH_LONG);
-//            toast.show();
-//
-//        }
+        String lbp_input=lbp_rate.getText().toString();
+        String usd_input=usd_rate.getText().toString();
+
+        if (lbp_input!=null&&usd_input.isEmpty()) {
+            int result = Integer.parseInt(lbp_input) /40000;
+            Toast toast = Toast.makeText(getApplicationContext(), "the equivalent to this amount in usd is :" + result, Toast.LENGTH_LONG);
+            toast.show();
+        }
+
+        else if(usd_input!=null&&lbp_input.isEmpty()){
+            int result=Integer.parseInt(usd_input)*40000;
+            Toast toast=Toast.makeText(getApplicationContext(),"the equivalent to this amount in lbp is :"+result, Toast.LENGTH_LONG);
+            toast.show();
+        }
+        else if(lbp_input!=null&&usd_input!=null)
+        {
+
+            Toast toast=Toast.makeText(getApplicationContext(),"Invalid input please fill only one of the boxes ", Toast.LENGTH_LONG);
+            toast.show();
+
+        }
 
     }
 
--- app/src/main/java/com/lau/currencyconverter/page1.java
+++ app/src/main/java/com/lau/currencyconverter/page1.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class page1 extends AppCompatActivity {
     EditText username;
@@ -22,10 +23,13 @@ public class page1 extends AppCompatActivity {
     public void login(View v) {
         String username1=username.getText().toString();
         String password1=password.getText().toString();
-        if (username1=="user.leb"&&password1=="123456"){
-            Intent intent = new Intent(this, page1.class);
+        if (username1.equals("user.leb")&&password1.equals("123456")){
+            Intent intent = new Intent(this, MainActivity.class);
             startActivity(intent);
-
+        }
+        else{
+            Toast toast=Toast.makeText(getApplicationContext(),"wrong username or password ", Toast.LENGTH_LONG);
+            toast.show();
         }
     }
 
