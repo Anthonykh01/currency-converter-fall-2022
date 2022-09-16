73b1fc318aca3fa0f8d858e834e22711e13ac526
anthonyykh <anthonykh01.ak@gmail.com>
2022-09-16 19:17:31 +0300

    added back end to calculate rate depending on which parameter is filled

--- app/src/main/java/com/lau/currencyconverter/MainActivity.java
+++ app/src/main/java/com/lau/currencyconverter/MainActivity.java
@@ -18,7 +18,20 @@ public class MainActivity extends AppCompatActivity {
         usd_rate=(EditText) findViewById(R.id.usd_rate);
     }
     public void Pressed_button(View v) {
+        String lbp_input=lbp_rate.getText().toString();
+        String usd_input=usd_rate.getText().toString();
 
+        if (lbp_input!=null&&usd_input.isEmpty()) {
+            int result = Integer.parseInt(lbp_input) /40000;
+            Toast toast = Toast.makeText(getApplicationContext(), "the equivalent to this amount in usd is :" + result, Toast.LENGTH_LONG);
+            toast.show();
+        }
+
+        else if(usd_input!=null&&lbp_input.isEmpty()){
+            int result=Integer.parseInt(usd_input)*40000;
+            Toast toast=Toast.makeText(getApplicationContext(),"the equivalent to this amount in lbp is :"+result, Toast.LENGTH_LONG);
+            toast.show();
+        }
     }
 
 }
